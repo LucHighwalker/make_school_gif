4c0cc07bebea721250a0e949023da945080b614c
LucHighwalker <luc.highwalker@gmail.com>
2018-07-26 19:31:19 -0700

    - Cleanup.

--- app.js
+++ app.js
@@ -2,35 +2,24 @@ var express = require('express');
 var app = express();
 
 var exphbs = require('express-handlebars');
-
 var giphy = require('giphy-api')();
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-    console.log('Gif Search listening on port localhost:3000!');
+app.listen(4200, function () {
+    console.log('Gif Search listening on port localhost:4200!');
 });
 
 app.get('/', function (req, res) {
-    giphy.search(req.query.term, function (err, response) {
-        var giphyRes = response ? response : { data: null };
+    var input = req.query.term ? req.query.term : ' ';
 
-        res.render('home', { gifs: giphyRes.data })
+    giphy.search(input, function (err, response) {
+        res.render('home', { gifs: response.data });
+
+        if (err !== null) {
+            console.error(err);
+        }
     });
-});
-
-app.get('/hello-gif', function (req, res) {
-    var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif';
-    res.render('hello-gif', { gifUrl: gifUrl })
-});
-
-app.get('/hello-world', function (req, res) {
-    res.send('Hello World');
-});
-
-app.get('/greetings/:name', function (req, res) {
-    var name = req.params.name;
-    res.render('greetings', { name: name });
 });
