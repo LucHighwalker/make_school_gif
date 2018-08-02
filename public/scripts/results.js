f5d57e590acf6b209c1f5d3835858b77cda243e2
LucHighwalker <luc.highwalker@gmail.com>
2018-08-01 20:44:09 -0700

    - Gifs toggle favorites on click.

--- public/scripts/results.js
+++ public/scripts/results.js
@@ -1,16 +1,35 @@
-var addFavorite = function (gif) {
+var findIndex = function (array, obj) {
+    var index = -1;
+    for (var i = 0; i < array.length; i++) {
+        if (array[i].id === obj.id){
+            index = i;
+            break;
+        }
+    }
+    return index;
+}
+
+var toggleFavorite = function (gif) {
+    console.log(gif);
     var docRef = firestore.collection('users').doc('testuser');
 
     docRef.get().then((doc) => {
         var data = doc.data();
         var favs = data.favorites;
 
-        favs.push(gif);
+        var exists = findIndex(favs, gif);
+
+        if (exists >= 0) {
+            favs.splice(exists, 1);
+        } else {
+            favs.push(gif);
+        }
 
         data.favorites = favs;
 
         docRef.set(data).then(() => {
             console.log('updated favorites');
+            location.reload();
         }).catch((error) => {
             console.error('error updating favorites: ' + error);
         });
