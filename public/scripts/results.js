24c6f66c2d32682ee38c172deaad8c62b782e234
LucHighwalker <luc.highwalker@gmail.com>
2018-08-09 11:52:47 -0700

    - Can now navigate pages by swiping left or right.

--- public/scripts/results.js
+++ public/scripts/results.js
@@ -1,21 +1,12 @@
+const swipeDiff = 20;
+
+document.addEventListener('touchstart', touchStart, false);
+document.addEventListener('touchmove', touchMove, false);
+
 var focusing = false;
+var touchDown = null;
 
-const focusGif = function (gifID) {
-    if (focusing === false) {
-        focusing = true;
-        var curURL = window.location.href;
-        var joinSymbol = '?';
-
-        if (curURL.includes('?')) {
-            joinSymbol = '&';
-        }
-
-        history.pushState(null, null, curURL + joinSymbol + $.param({ focus: gifID }));
-        window.location.reload();
-    }
-}
-
-const changePage = function (curPage, mod) {
+const changePage = function (mod) {
     var curURL = window.location.href;
     var newURL = '';
     var pageParam = $.param({ page: null });
@@ -39,10 +30,38 @@ const changePage = function (curPage, mod) {
     }
 }
 
-const prevPage = function (curPage) {
-    changePage(curPage, -1);
+const focusGif = function (gifID) {
+    if (focusing === false) {
+        focusing = true;
+        var curURL = window.location.href;
+        var joinSymbol = '?';
+
+        if (curURL.includes('?')) {
+            joinSymbol = '&';
+        }
+
+        history.pushState(null, null, curURL + joinSymbol + $.param({ focus: gifID }));
+        window.location.reload();
+    }
 }
 
-const nextPage = function (curPage) {
-    changePage(curPage, 1);
-}
+function touchStart(event) {
+    touchDown = event.touches[0].clientX;
+};
+
+function touchMove(event) {
+    if (!touchDown) {
+        return;
+    }
+
+    var touchUp = event.touches[0].clientX;
+    var diff = touchDown - touchUp;
+
+    if (nextPageAvail && diff > swipeDiff) {
+        changePage(1);
+    } else if (prevPageAvail && diff < -swipeDiff) {
+        changePage(-1);
+    }
+
+    touchDown = null;
+};
